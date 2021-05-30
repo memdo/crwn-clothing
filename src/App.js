caa0a2e0c17615f5a63ad43ae7a67c696a218d10
M. Berkay Düzenli <memdo@protonmail.com>
2021-05-30 05:12:36 +0300

    Separated components, added background images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import HomePage from './homepage.component'
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  return (
-    <div>
-      <HomePage/>
-    </div>
-  );
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
