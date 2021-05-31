16abb4b838149d876f8188c434538679ad0f0040
M. Berkay Düzenli <memdo@protonmail.com>
2021-05-31 04:09:45 +0300

    Added little navigation with withRouter()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = (props) => (
+    <div>
+        <h1>{props.match.params.subreddit}</h1>
+        <Link to="/">Homepage</Link>
+        <h1>HATS PAGE</h1>
+    </div>
+);
 
 function App() {
     return (
         <div>
-            <HomePage />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/hats" component={HatsPage} />
+            </Switch>
         </div>
     );
 }
